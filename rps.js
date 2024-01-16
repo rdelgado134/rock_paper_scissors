7de9b9ad03dc6cc43db3773e5ff050e373d2eb11
Richard Delgado <rdelgado134@gmail.com>
2024-01-15 16:19:47 -0800

    play RPS function added to rps.js.

--- rps.js
+++ rps.js
@@ -12,18 +12,42 @@ function getComputerChoice()
 {
    let choiceNum = Math.floor(Math.random() * 3);
 
-   if(choiceNum == 0) {return "Rock";}
-   else if(choiceNum == 1) {return "Paper";}
-   else {return "Scissors";}
+   if(choiceNum == 0) {return "ROCK";}
+   else if(choiceNum == 1) {return "PAPER";}
+   else {return "SCISSORS";}
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-function playRPS(playerSelection, computerSelection)
-{
-    /*returns a string that declares the winner of the
+
+  /*returns a string that declares the winner of the
     round like: "You Lose! Paper beats Rock"
     */
+function playRPS(playerSelection, computerSelection)
+{
+    playerSelection = prompt("Choose you discipline.");
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = getComputerChoice();
+
+    if(playerSelection == computerSelection) {return " TIE!"}
+
+    if(playerSelection == "ROCK")
+    {
+        if(computerSelection == "PAPER") { return "You lose! Paper beats Rock.";}
+        else if(computerSelection == "SCISSORS") {return "You Win! Rock beats Scissors.";}
+    }
+
+    if(playerSelection == "PAPER")
+    {
+        if(computerSelection == "SCISSORS") { return "You lose! Scissors beats Paper.";}
+        else if(computerSelection == "PAPER") {return "You Win! Paper beats Rock.";}
+    }
+    
+    if(playerSelection == "SCISSORS")
+    {
+        if(computerSelection == "PAPER") { return "You lose! Scissors beat Paper.";}
+        else if(computerSelection == "ROCK") {return "You Win! Rock beats Scissors.";}
+    }
 }
