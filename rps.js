e4f17dc9958a0d6b4020d46bdc6598b45386151d
Richard Delgado <rdelgado134@gmail.com>
2024-03-05 04:55:08 -0800

    Added buttons for the users choice instead of the prompt. Should probably add a clear button this works for now.

--- rps.js
+++ rps.js
@@ -7,6 +7,11 @@ function getComputerChoice()
    else if(choiceNum == 1) {return "PAPER";}
    else {return "SCISSORS";}
 }
+const userScore = document.createElement("div");
+const comScore = document.createElement("div");
+
+userScore.textContent = 0;
+comScore.textContent = 0;
 
 /* Generates a number from 0-2 which will help determine the computers choice.*/
 function getRandomInt(max) {
@@ -14,40 +19,121 @@ function getRandomInt(max) {
   }
 
 
+  const rockButton = document.getElementById("rock");
+  const paperButton = document.getElementById("paper");
+  const scissorsButton = document.getElementById("scissors");
+  rockButton.addEventListener('click', playRock);
+  paperButton.addEventListener("click", playPaper);
+  scissorsButton.addEventListener("click", playScissors);
+
   /*returns a string that declares the winner of the
     round like: "You Lose! Paper beats Rock"
     */
    
-function playRPS(playerSelection, computerSelection)
+function playRock()
 {
-    playerSelection = prompt("Choose you discipline.");
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = getComputerChoice();
+    const playerSelection = "ROCK";
+    const computerSelection = getComputerChoice();
+    const result = document.createElement("div");
 
-    if(playerSelection == computerSelection) {return " TIE!"}
+    if(playerSelection == computerSelection) {result.textContent = " TIE!"}
+    
+    if(computerSelection == "PAPER")
+     {
+        result.textContent = "You lose! Paper beats Rock.";
+        comScore.textContent++;
+    }
+    else if(computerSelection == "SCISSORS") 
+    {
+        result.textContent = "You Win! Rock beats Scissors.";
+        userScore.textContent++;
+    }
+    updateScore();
+
+        document.getElementById("resultsHolder").appendChild(result);
+}
+function playPaper()
+{
+    const playerSelection = "PAPER"
+    const computerSelection = getComputerChoice();
+    const result = document.createElement("div");
+
+    if(playerSelection == computerSelection) {result.textContent = " TIE!"}
+
+    if(computerSelection == "SCISSORS") 
+    {
+        result.textContent ="You lose! Scissors beats Paper.";
+        comScore.textContent++;
+    }
+    else if
+    (
+        computerSelection == "ROCK") {result.textContent ="You Win! Paper beats Rock.";
+        userScore.textContent++;
+    }
+    updateScore();
+
+    document.getElementById("resultsHolder").appendChild(result);
+}
+function playScissors()
+{
+    const playerSelection = "SCISSORS"
+    const computerSelection = getComputerChoice();
+    const result = document.createElement("div");
+
+    if(playerSelection == computerSelection) {result.textContent = " TIE!"}
+
+    if(computerSelection == "ROCK") 
+    {
+         result.textContent = "You lose! Rock beats Scissors.";
+         comScore.textContent++;
+    }
+    else if (computerSelection == "PAPER") 
+    {
+        result.textContent ="You Win! Scissors beats Paper.";
+        userSCore++;
+    }
+
+    updateScore();
+    document.getElementById("resultsHolder").appendChild(result);
+}
+function updateScore()
+{
+    document.getElementById("userScore").appendChild(userScore);
+    document.getElementById("comScore").appendChild(comScore);
+}
+
+/*function playRPS(playerSelection, computerSelection)
+{
+    playerSelection = "ROCK"
+    computerSelection = getComputerChoice();
+    const result = document.createElement("div");
+
+    if(playerSelection == computerSelection) {result.textContent = " TIE!"}
 
     if(playerSelection == "ROCK")
     {
-        if(computerSelection == "PAPER") { return "You lose! Paper beats Rock.";}
-        else if(computerSelection == "SCISSORS") {return "You Win! Rock beats Scissors.";}
+        if(computerSelection == "PAPER") {result.textContent = "You lose! Paper beats Rock.";}
+        else if(computerSelection == "SCISSORS") {result.textContent = "You Win! Rock beats Scissors.";}
     }
 
     if(playerSelection == "PAPER")
     {
-        if(computerSelection == "SCISSORS") { return "You lose! Scissors beats Paper.";}
-        else if(computerSelection == "ROCK") {return "You Win! Paper beats Rock.";}
+        if(computerSelection == "SCISSORS") {result.textContent ="You lose! Scissors beats Paper.";}
+        else if(computerSelection == "ROCK") {result.textContent ="You Win! Paper beats Rock."; }
     }
     
     if(playerSelection == "SCISSORS")
     {
-        if(computerSelection == "ROCK") { return "You lose! Rock beats Scissors.";}
-        else if(computerSelection == "PAPER") {return "You Win! Scissors beats Paper.";}
+        if(computerSelection == "ROCK") { result.textContent = "You lose! Rock beats Scissors.";}
+        else if(computerSelection == "PAPER") {result.textContent ="You Win! Scissors beats Paper.";}
     }
+    document.getElementById("resultsHolder").appendChild(result);
 }
+*/
 
 
 /*game() runs a best of 5 rock paper scissors game*/
-function game()
+/*function game()
 {
     let comScore = 0;
     let playerScore = 0;
@@ -70,4 +156,4 @@ function game()
     if (comScore > playerScore) {alert("You lost the game!");}
     else {alert("You won the game!");}
  
-}
+}*/
